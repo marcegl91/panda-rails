ed05cd3594e868d24881c8fc5fbbe40dd15561e9
Marcelo Guzman <marcegl91@gmail.com>
2019-08-31 04:11:38 -0300

    Fix serializers and seeds

--- app/serializers/donations_serializer.rb
+++ app/serializers/donations_serializer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class DonationsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :photo, :category, :state, :requester_id, :offerer_id
+  attributes :id, :name, :description, :photo, :category, :state
+
+  has_one :requester, serializer: UsersSerializer
+  has_one :offerer, serializer: UsersSerializer
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,17 @@
   Category.create(name: x)
 end
 
-User.create(email: 'test0@test.com', nickname: 'user0', password: 'testtest', latitude: -34.564090, longitude: -58.479071)
+User.create(name: 'Test User 0', email: 'test0@test.com', nickname: 'user0', password: 'testtest', latitude: -34.564090, longitude: -58.479071)
 
-User.create(email: 'test1@test.com', nickname: 'user1', password: 'testtest', latitude: -34.564075, longitude: -58.469277)
+User.create(name: 'Test User 1', email: 'test1@test.com', nickname: 'user1', password: 'testtest', latitude: -34.564075, longitude: -58.469277)
 
-User.create(email: 'test2@test.com', nickname: 'user2', password: 'testtest', latitude: -34.607201, longitude: -58.421511)
+User.create(name: 'Test User 2', email: 'test2@test.com', nickname: 'user2', password: 'testtest', latitude: -34.607201, longitude: -58.421511)
 
-User.create(email: 'test3@test.com', nickname: 'user3', password: 'testtest', latitude: -34.612640, longitude: -58.413379)
+User.create(name: 'Test User 3', email: 'test3@test.com', nickname: 'user3', password: 'testtest', latitude: -34.612640, longitude: -58.413379)
 
-User.create(email: 'test4@test.com', nickname: 'user4', password: 'testtest', latitude: -34.613063, longitude: -58.387973)
+User.create(name: 'Test User 4', email: 'test4@test.com', nickname: 'user4', password: 'testtest', latitude: -34.613063, longitude: -58.387973)
 
-User.create(email: 'test5@test.com', nickname: 'user5', password: 'testtest', latitude: -34.562809, longitude: -58.481830)
+User.create(name: 'Test User 5', email: 'test5@test.com', nickname: 'user5', password: 'testtest', latitude: -34.562809, longitude: -58.481830)
 
 Donation.create(name: 'A donation', description: 'Some text about that donation',
                 requester: User.first, offerer: User.second, state: 0, category: Category.first)
