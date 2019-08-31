9019bea2f2874d034d23e5ce1807a8d61a91ac47
Marcelo Guzman <marcegl91@gmail.com>
2019-08-30 22:06:12 -0300

    Add description to categories

--- app/serializers/categories_serializer.rb
+++ app/serializers/categories_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class CategoriesSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@
   Category.create(name: x)
 end
 
-User.create(name: 'test@test.com', password: 'testtest')
-User.create(name: 'test1@test.com', password: 'testtest')
+User.create(email: 'test@test.com', password: 'testtest')
+User.create(email: 'test1@test.com', password: 'testtest')
 
 Donation.create(name: 'A donation', description: 'Some text about that donation',
                 requester: User.first, offerer: User.second)
