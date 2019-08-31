76b0801609f05ff2f316fd6fd28f05819b1bd42e
Marcelo Guzman <marcegl91@gmail.com>
2019-08-31 02:39:09 -0300

    Add request-donations, offer-donations and nearby-users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,25 @@ module Api
   module V1
     class UsersController < ApiController
       def show
-        render json: User.find(params[:id]), serializer: UsersSerializer
+        render json: user, serializer: UsersSerializer
+      end
+
+      def request_donations
+        render json: user.requests, each_serializer: DonationsSerializer
+      end
+
+      def offer_donations
+        render json: user.offers, each_serializer: DonationsSerializer
+      end
+
+      def nearby_users
+        render json: user.nearbys(10, unit: :km), each_serializer: UsersSerializer
+      end
+
+      private
+
+      def user
+        User.find(params[:id])
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: [:show] do
+        member do
+          get 'request-donations'
+          get 'offer-donations'
+          get 'nearby-users'
+        end
+      end
+
       resources :categories, only: [:index]
       resources :donations, only: [:index, :show, :create]
     end
